a9ca1aebf6988123f5f4f99e1ffcd6ca1909da4f
Aman <amank1601@gmail.com>
2018-09-02 15:22:00 +0530

    Initial Commit. Added Splash Screen.

---
+++ app/src/main/java/com/example/home/smartcityjbp/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.home.smartcityjbp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        final Context context = this;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(context, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2500);
+    }
+}
