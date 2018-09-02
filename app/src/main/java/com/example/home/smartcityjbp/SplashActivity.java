c0205205f9333f122038299c0a990bb3c7bab536
Aman <amank1601@gmail.com>
2018-09-02 22:15:43 +0530

    Theme changed

--- app/src/main/java/com/example/home/smartcityjbp/SplashActivity.java
+++ app/src/main/java/com/example/home/smartcityjbp/SplashActivity.java
@@ -24,6 +24,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 2500);
+        }, 2 * 1000);
     }
 }
