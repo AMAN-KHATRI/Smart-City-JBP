2dd096a5bb4f097d138e86ce4764ed518c1cc150
Aman <amank1601@gmail.com>
2018-09-05 14:22:56 +0530

    Add - Image Picker and save button

--- app/src/main/java/com/example/home/smartcityjbp/UpdateProfileActivity.java
+++ app/src/main/java/com/example/home/smartcityjbp/UpdateProfileActivity.java
@@ -1,8 +1,14 @@
 package com.example.home.smartcityjbp;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
@@ -10,6 +16,11 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class UpdateProfileActivity extends AppCompatActivity {
 
@@ -17,6 +28,9 @@ public class UpdateProfileActivity extends AppCompatActivity {
     Spinner genderSpinner;
     EditText phoneNumberEt;
     int mGender = 0;    //0 is unknown, 1 is male and 2 is female
+    final int IMAGE_REQUEST_CODE = 111;
+    CircleImageView profileIv;
+    FloatingActionButton saveFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +40,17 @@ public class UpdateProfileActivity extends AppCompatActivity {
         setupSpinner();
 
         setupPhoneNumber();
+
+        setupProfileImage();
+
+        saveFab = findViewById(R.id.save_fab);
+        saveFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(UpdateProfileActivity.this, "Save successful", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
     }
 
     private void setupSpinner() {
@@ -64,5 +89,34 @@ public class UpdateProfileActivity extends AppCompatActivity {
         sharedPreferencesForPhoneNumber = PreferenceManager.getDefaultSharedPreferences(this);
 
         phoneNumberEt.setText(sharedPreferencesForPhoneNumber.getString("phone_number", "0000"));
+    }
+
+    private void setupProfileImage() {
+        profileIv = findViewById(R.id.profile_image);
+
+        profileIv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, IMAGE_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        Bitmap image = null;
+        if (requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+
+            try {
+                image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+            } catch (IOException e) {
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            profileIv.setImageBitmap(image);
+        }
     }
 }
